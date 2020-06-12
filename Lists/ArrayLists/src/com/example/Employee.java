6eb23d03b1df0d7c7b97315ead80bbb025a2eace
Idris Khan <idris.khan303@gmail.com>
2020-06-12 13:35:19 +0100

    ArrayLists lecture completed

---
+++ Lists/ArrayLists/src/com/example/Employee.java
@@ -0,0 +1,63 @@
+package com.example;
+
+import java.util.Objects;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public Employee(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id &&
+                this.firstName.equalsIgnoreCase(((Employee) o).firstName) &&
+                this.lastName.equalsIgnoreCase(((Employee) o).lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
