a0471080b0b07bb2412df2ceb6752475193e7243
Idris Khan <idris.khan303@gmail.com>
2020-07-11 15:32:33 +0100

    Added the code for get, min, max and delete for cases 1 and 2, with all the notes for delete

--- Trees/BinarySearchTrees/src/com/example/Main.java
+++ Trees/BinarySearchTrees/src/com/example/Main.java
@@ -7,11 +7,34 @@ public class Main {
         intTree.insert(25);
         intTree.insert(20);
         intTree.insert(15);
+        intTree.insert(17);
         intTree.insert(27);
         intTree.insert(30);
         intTree.insert(29);
         intTree.insert(26);
         intTree.insert(22);
         intTree.insert(32);
+
+        System.out.println("---IN ORDER---");
+        intTree.inOrder();
+
+        System.out.println("\n\n---PRE ORDER---");
+        intTree.preOrder();
+
+        System.out.println("\n\n---POST ORDER---");
+        intTree.postOrder();
+
+        System.out.println("\n\n---GET---");
+        System.out.printf("Returned Value := %s\n", intTree.get(15));
+        System.out.printf("Returned Value := %s\n", intTree.get(50));
+        System.out.printf("Returned Value := %s\n", intTree.get(27));
+        System.out.printf("Returned Value := %s\n", intTree.get(30));
+
+        System.out.println("\n---MIN---");
+        System.out.println(intTree.min());
+
+        System.out.println("\n---MAX---");
+        System.out.println(intTree.max());
     }
+
 }
--- Trees/BinarySearchTrees/src/com/example/Tree.java
+++ Trees/BinarySearchTrees/src/com/example/Tree.java
@@ -18,6 +18,164 @@ public class Tree {
         }
     }
 
+    public TreeNode get(int value) {
+        if(!isEmpty()) {
+            return root.get(value);
+        }
+        return null;
+    }
+
+    public void delete(int value) {
+        root = delete(root, value); // returns the root of the subtree
+    }
+
+    private TreeNode delete(TreeNode subTreeRoot, int value) {
+        // test to see empty TreeNodeList return null essentially
+        if(subTreeRoot == null) {
+            return subTreeRoot;
+        }
+        /* the 2 iffys handle the traversing of the tree always making sure that subtree root node becomes the child
+           of the node we are checking against
+
+           The moment when the node equal to the data, we have found the node we are looking for
+
+                                            25
+                                           /  \
+                                          20   27
+                                         /       \
+                                        15        30
+                                          \
+                                           17
+
+            Subtree root node when entered into the delete method is 25 - the root node
+            when the method is recursively called it will be passing in either the left/ right child node
+            so in this case 20 or 27 as the subTreeRoot - so as we go further down in the rabbit hole of recursion we
+            always have a reference to the subTreeRoot Node or basically its parent node
+
+
+        */
+        if(value < subTreeRoot.data) {
+            subTreeRoot.leftChild = delete(subTreeRoot.leftChild, value);
+        } else if (value > subTreeRoot.data) {
+            subTreeRoot.rightChild = delete(subTreeRoot.rightChild, value);
+        // We have found the node we are looking for as its one of the children of the subTreeRoot
+        // now there are  3 cases that can happen
+        /*
+        * 1. Its a leaf node
+        * 2. Its a left/ right child node of its subtree root node
+        * 3 There are 2 children
+        *
+        * */
+        } else {
+            /*
+            * In this code we cover the first 2 cases
+            *                               25
+                                           /  \
+                                          20   27
+                                         /       \
+                                        15        30
+                                          \
+                                           17
+            *
+            * If we have a tree like this and we want to do case 1, where we target a leaf node
+            * root = delete(25, 17)
+            *   In delete we check if 17 < 25: TRUE
+            *   So we set 25's leftChild (20) to be equal to delete(20, 17)
+            *
+            * Node 25 Left Child (Node 20) = (20, 17)
+            *   17 < 20: TRUE
+            *   So we set 20's leftChild to be equal to delete(15, 17)
+            *
+            * Node 20 Left Child (Node 15) = (15, 17)
+            *   17 > 15: TRUE
+            *   So we set 15's rightChild to be equal to delete(17, 17)
+            *
+            * Node 15 Right Child (Node 17) = (17, 17)
+            *   17 < 17 FALSE
+            *   17 > 17 FALSE
+            *
+            * We found the node, so we go to the else block and the first check for leftChild == null is true
+            * (rightChild == null is true but the first one condition is ran first) and it will return the leaf
+            * node's rightChild - which in this case is null
+            *
+            * delete(17, 17) = null
+            * Node 17 = null
+            * Node 15 Right Child = null
+            * Node 20 Left Child = Node 15 (with the new edge link, that its right child is null)
+            * Node 25 Left Child = Node 20 (with the new edge link from 15)
+            *
+            * And all the way to the top
+            * root = delete(root, value)
+            * root now has new reference type connections, as we traversed the left subtree, the right subtree
+            * connections are still the same, but its left subtree connections are the one that has changed
+            *
+            * This code also holds true to when we want to delete a node that has one child
+            * root = delete(25, 15)
+            *   In delete we check if 15 < 25: TRUE
+            *   So we set 25's leftChild (20) to be equal to delete(20, 17)
+            *
+            * Node 25 Left Child (Node 20) = (20, 15)
+            *   15 < 20: TRUE
+            *   So we set 20's leftChild to be equal to delete(15, 17)
+            *
+            * Node 20 Left Child (Node 15) = (15, 15)
+            *   15 < 15: FALSE
+            *   15 > 15: FALSE
+            *   We have found the node
+            *
+            * We run the else block of code, where we check if it has a left or right child, and simply return it
+            *
+            * so (15, 15) = Node 17
+            * Node 20 Left Child (Node 15) = Node 17
+            * Node 25 Left Child (Node 20) = Node 20 (With its new connection that Node 15 has been replaced with
+            * Node 17)
+            *
+            * root = delete(root, value)
+            * With the new Node list connections
+            * */
+            if(subTreeRoot.leftChild == null) {
+                return subTreeRoot.rightChild;
+            } else if (subTreeRoot.rightChild == null) {
+                return subTreeRoot.leftChild;
+            }
+        }
+        return null;
+    }
+
+    public TreeNode min() {
+        if(!isEmpty()) {
+            return root.min();
+        }
+        return null;
+    }
+
+    public TreeNode max() {
+        if(!isEmpty()) {
+            return root.max();
+        }
+        return null;
+    }
+
+    public void inOrder() {
+        if(!isEmpty()) {
+            System.out.println("Data:");
+            root.traverseInOrder();
+        }
+    }
+
+    public void preOrder() {
+        if(!isEmpty()) {
+            System.out.println("Data:");
+            root.traversePreOrder();
+        }
+    }
+
+    public void postOrder() {
+        if(!isEmpty()) {
+            System.out.println("Data:");
+            root.traversePostOrder();
+        }
+    }
 
     public boolean isEmpty() {
         return root == null;
@@ -69,6 +227,98 @@ public class Tree {
                 }
             }
         }
+
+        public TreeNode get(int value) {
+            if(value == data) {
+                return this;
+            }
+
+            if(value < data) {
+                if(leftChild != null) {
+                    return leftChild.get(value);
+                }
+
+            } else {
+                if(rightChild != null) {
+                    return rightChild.get(value);
+                }
+            }
+
+            return null;
+        }
+
+        public TreeNode min() {
+            if(leftChild != null) {
+                return leftChild.min();
+            }
+            return this;
+        }
+
+        public TreeNode max() {
+            if(rightChild != null) {
+                return rightChild.max();
+            }
+            return this;
+        }
+
+        public void traverseInOrder() {
+            /*
+            * How this method works?
+            * - We check if the the current node (the subtree root node) has any left and right children
+            *
+            * - if so, lets recursively call the traverseInOrder method again but on the left child first, this will
+            * push each method call onto the call stack
+            *
+            * - We keep recursively calling this method till we come across a leaf node (one that does not have
+            *   children)
+            *
+            * - When we come across a leaf node, the check to see if the leftChild is not equal to null will fail and
+            *  then go to the print statement where it will output the node data
+            *
+            * - The code will then go to check if the leaf node has any right child - which will be false so the
+            * program execution of this method will end
+            *
+            * - The call stack will then pop the previous call to traverseInOrder - which will be the parent of the
+            * leaf node, its code will then go to the print statement and print the node data
+            *
+            * - It will then check if there is any right child, if so it will recursively call the method but for the
+            *  right child
+            *
+            * - Until it comes to a point where it hits another leaf node - and repeats the process for checking
+            * children on a leaf node
+            * */
+            if(leftChild != null) {
+                leftChild.traverseInOrder();
+            }
+            System.out.printf("%d, ", data);
+            if(rightChild != null) {
+                rightChild.traverseInOrder();
+            }
+        }
+
+        public void traversePreOrder() {
+            System.out.printf("%d, ", data);
+            if(leftChild != null) {
+                leftChild.traversePreOrder();
+            }
+
+            if(rightChild != null) {
+                rightChild.traversePreOrder();
+            }
+        }
+
+        public void traversePostOrder() {
+            if(leftChild != null) {
+                leftChild.traversePostOrder();
+            }
+
+            if(rightChild != null) {
+                rightChild.traversePostOrder();
+            }
+            System.out.printf("%d, ", data);
+        }
+
+
 
         @Override
         public String toString() {
